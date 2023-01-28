fed9d67f0326e02f3c94e6e544df84110df7b8d6
Your Name <you@example.com>
2023-01-28 09:55:47 +0530

    addlogin

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+
+import 'login_page.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("HOME PAGE"),),
+      body: InkWell(
+          onTap: (){},
+          child: Center(child: Text("HOME PAGE"),)),
+
+
     );
   }
 }
